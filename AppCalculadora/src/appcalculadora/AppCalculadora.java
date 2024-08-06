56f6039ab04ef5a4d1cf132c59acbb02e351b5ad
Daniel Macedo <164789807+Daniel-Macedo-dev@users.noreply.github.com>
2024-08-06 13:36:35 -0300

    Add files via upload

---
+++ AppCalculadora/src/appcalculadora/AppCalculadora.java
@@ -0,0 +1,17 @@
+
+package appcalculadora;
+
+/**
+ *
+ * @author daniel.msilva59
+ */
+public class AppCalculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
